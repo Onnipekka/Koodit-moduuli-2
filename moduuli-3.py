7d17a080c6c726a19a6533d84570c4a60af15952
Onnipekka <onnipekka.johannes@gmail.com>
2025-01-21 17:34:00 +0200

    Add files via upload

---
+++ moduuli-3.py
@@ -0,0 +1,41 @@
+#kuha = float(input("Anna kuhan mitta: "))
+#if kuha >= 37:
+#    print("Kuha ei ole alamittainen.")
+#if kuha < 37:
+#    kuha1 = kuha-37
+#    print(f"Kuha on {kuha1} senttimetriä liian lyhyt, ja se lasketaan takaisin veteen.")
+
+
+#hytti = input("Anna hyttisi luokka (Hyttiluokat ovat LUX, A, B tai C): ")
+#if hytti == "LUX":
+#    print("LUX on parvekkeellinen hytti yläkännella.")
+#elif hytti == "A":
+#    print("Hyttisi on ikkunallinen hytti autokannen yläpuolella.")
+#elif hytti == "B":
+#    print("Hyttisi on ikkunaton hytti autokannen yläpuolella.")
+#elif hytti == "C":
+#    print("Hyttisi on ikkunaton hytti autokannen alapuolella.")
+#else:
+#    print("Virheellinen hyttiluokka.")
+
+
+#sukupuoli = input("Anna biologinen sukupuolesi (mies/nainen): ")
+#hg = int(input("Anna hemoglobiiniarvosi: "))
+#if sukupuoli == "nainen" and 117<= hg <=175:
+#    print("Hemoglobiiniarvosi on normaali.")
+#elif sukupuoli == "mies" and 134<= hg <=195:
+#    print("Hemoglobiiniarvosi on normaali.")
+#elif sukupuoli == "mies" and hg > 195:
+#    print("Hemoglobiiniarvosi on korkea.")
+#elif sukupuoli == "mies" and hg < 134:
+#    print("Hemoglobiiniarvosi on matala")
+#elif sukupuoli == "nainen" and hg > 175:
+#    print("Hemoglobiiniarvosi on korkea.")
+#elif sukupuoli == "nainen" and hg < 117:
+#    print("Hemoglobiiniarvosi on matala.")
+
+
+
+
+
+
