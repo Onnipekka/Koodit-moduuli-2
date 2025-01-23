91925b46f9f556d39e6d8a033c2f113434eec087
Onnipekka <onnipekka.johannes@gmail.com>
2025-01-23 12:46:26 +0200

    Add files via upload

---
+++ moduuli-4.py
@@ -0,0 +1,71 @@
+#kerrat = int(input("Montako kertaa tervehditään? : "))
+#tehdyt = 0
+#while tehdyt<kerrat:
+#    print("Hyvää huomenta!")
+#    tehdyt = tehdyt + 1
+from random import random
+
+#komento = input("Anna komento: ")
+#while komento!= "lopeta":
+#    print("Suoritan toiminnon " + komento)
+#    komento = input("Anna komento: ")
+#print("Toiminnot lopetettu.")
+
+#luku = 1
+#while luku<=1000:
+#    if luku % 3:
+#        print(luku)
+#    luku = luku + 1
+
+#luku = int(input("Anna tuumamäärä: "))
+#luku = luku * 2.54
+#while luku > 0:
+#    print(f"{luku}cm")
+#    luku = int(input("Anna tuumamäärä: "))
+#    luku = luku * 2.54
+#    if luku <= 0:
+#        print("Anna positiivinen luku")
+
+#import math
+#pienin = math.inf
+#suurin = math.inf
+#luku_str = input("Anna ensimmäinen luku, tyhjä lopettaa: ")
+#while luku_str != "":
+#    luku = float(luku_str)
+#    if luku < pienin:
+#        pienin = luku
+#    elif luku > suurin:
+#        suurin = luku
+#    luku_str = input("Anna seuraava luku: ")
+#print(f"Antamistasi luvuista suurin oli {suurin} ja pienin oli {pienin}.")
+
+#import random
+#noppa = random.randint(1,10)
+#noppa1 = int(input("Arvaa luku (1-10): "))
+#while noppa1 > noppa or noppa1 < noppa:
+#    if noppa1 > noppa:
+#        print("Liian suuri luku.")
+#    elif noppa1 < noppa:
+#        print("Liian pieni luku.")
+#    noppa1 = int(input("Anna toinen luku: "))
+#else:
+#    print("Oikein!")
+
+#numero = 0
+#numero1 = 5
+#kay = "python"
+#sala = "rules"
+#while (numero < numero1):
+#    kay = input("Anna käyttäjätunnuksesi: ")
+#    sala = input("Anna salasanasi: ")
+#    if kay == "python" and sala == "rules":
+#        print("Tervetuloa!")
+#        break
+#    else:
+#        numero += 1
+#        print("Väärät tiedot syötetty.")
+#if numero == numero1:
+#    print("Pääsy evätty.")
+
+
+
